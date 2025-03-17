2cc7eebc6595ab835a28d5c032dd05f7e12fcc00
Ziol3k <jan_ziolo@wp.pl>
2025-03-17 17:03:46 +0100

    Ex 2 - Program WPF wykorzystujacy funkcje z zadania 1

---
+++ WpfApp1/WpfApp1/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public Class1()
+        {
+        }
+
+        public static string Potega(int a, int b, int c)
+        {
+            if (c <= 0)
+            {
+                return "trzeci argument jest mniejszy od 0";
+            }
+
+            if (b < 0)
+            {
+                return "wykladnik mniejszy od 0";
+            }
+
+            return Math.Pow(a, b).ToString();
+        }
+
+        public static int[] ZapiszWTablicy(int[] tablica, int mnoznik)
+        {
+            int[] nowaTablica = new int[tablica.Length];
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                nowaTablica[i] = tablica[i] * mnoznik;
+            }
+            return nowaTablica;
+        }
+
+        public static string PoleKola(double r)
+        {
+            if (r <= 0)
+            {
+                return "promien musi byc wiekszy od 0";
+            }
+
+            double pole = Math.PI * Math.Pow(r, 2);
+            return pole.ToString();
+        }
+
+        public static string SumaCyfr(int liczba)
+        {
+            if (liczba < 100 || liczba > 999)
+            {
+                return "podana liczba nie jest trzycyfrowa";
+            }
+
+            int suma = 0;
+            int temp = liczba;
+            while (temp > 0)
+            {
+                suma += temp % 10;
+                temp /= 10;
+            }
+
+            if (suma % 3 == 0)
+            {
+                return "liczba podzielna przez 3";
+            }
+            else
+            {
+                return "liczba nie jest podzielna przez 3";
+            }
+        }
+
+        public static string ZamienElementy(int[] tablica, int i, int j, int warunek)
+        {
+            if (i < 0 || i >= tablica.Length || j < 0 || j >= tablica.Length)
+            {
+                return "indeks spoza zakresu";
+            }
+
+            if (warunek > 0)
+            {
+                int temp = tablica[i];
+                tablica[i] = tablica[j];
+                tablica[j] = temp;
+            }
+
+            string wynik = "";
+            for (int k = 0; k < tablica.Length; k++)
+            {
+                wynik += tablica[k] + (k < tablica.Length - 1 ? ", " : "");
+            }
+
+            return wynik;
+        }
+    }
+}
+
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using ConsoleApp1;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Przycisk1_Click(object sender, RoutedEventArgs e)
+    {
+
+        int a = text_a.Text != "" ? int.Parse(text_a.Text) : 0;
+        int b = text_b.Text != "" ? int.Parse(text_b.Text) : 0;
+        int c = text_c.Text != "" ? int.Parse(text_c.Text) : 0;
+
+        int[] tablica = tablica_text.Text.Split(',').Select(int.Parse).ToArray();
+        tablica.OrderByDescending(x => x).First();
+
+        int mnoznik = mnoznik_text.Text != "" ? int.Parse(mnoznik_text.Text) : 0;
+        double r = promien_text.Text != "" ? double.Parse(promien_text.Text) : 0;
+        int liczba = liczba_text.Text != "" ? int.Parse(liczba_text.Text) : 0;
+        int i = i_text.Text != "" ? int.Parse(i_text.Text) : 0;
+        int j = j_text.Text != "" ? int.Parse(j_text.Text) : 0;
+        int warunek = warunek_text.Text != "" ? int.Parse(warunek_text.Text) : 0;
+
+        Etykieta1.Content = Class1.Potega(a, b, c);
+
+        Etykieta2.Content = Class1.ZapiszWTablicy(tablica, mnoznik);
+
+        Etykieta3.Content = Class1.PoleKola(r);
+
+        Etykieta4.Content = Class1.SumaCyfr(liczba);
+
+        Etykieta5.Content = Class1.ZamienElementy(tablica, i, j, warunek);
+
+    }
+
+
+}
