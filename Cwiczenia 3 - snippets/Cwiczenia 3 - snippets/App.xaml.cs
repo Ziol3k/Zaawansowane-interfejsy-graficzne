88139767ac6ec9f6b378b2f780e8e88279fbc15b
Ziol3k <jan_ziolo@wp.pl>
2025-03-24 13:21:17 +0100

    First snippet + usage

---
+++ Cwiczenia 3 - snippets/Cwiczenia 3 - snippets/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Cwiczenia_3___snippets;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ Cwiczenia 3 - snippets/Cwiczenia 3 - snippets/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia_3___snippets
+{
+    class Class1
+    {
+
+        static void Main(string[] args)
+        {
+            // wpisz kod 1
+        }
+
+
+    }
+}
